ed506a68133b577f1daeadb22efadb825230f59d
Bipin <bipin.yadav@inviziosolutions.com>
2024-04-15 17:20:58 +0530

    few lines commented

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 import Register from './components/register/Register';
@@ -6,7 +6,7 @@ import Register from './components/register/Register';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
       <Register/>
     </div>
   );
